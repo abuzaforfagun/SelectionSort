0ad762843a95b92c4f67d4639a6fee899e8850f6
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-12-02 02:32:46 +0600

    Implement selection sort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 21, 1, 34, 5, 10, 9 };
+            Sorter.DoSort(arr);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SelectionSort/Sorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    public class Sorter
+    {
+        private static void swap(int[] array, int newPosition, int currentPositioin)
+        {
+            var temp = array[newPosition];
+            array[newPosition] = array[currentPositioin];
+            array[currentPositioin] = temp;
+        }
+        public static int[] DoSort(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if(array[i] > array[j])
+                    {
+                        swap(array, j, i);
+                    }
+                }
+            }
+            return array; 
+        }
+    }
+}
